f222760192f9d60e7ad4a45623b7acc4d534984c
Jorge Calle <jcalleu18@gmail.com>
2021-10-28 17:52:25 -0400

    first commit

---
+++ target/classes/edu/citytech/tictactoe/TicTacToeMain.java
@@ -0,0 +1,26 @@
+package edu.citytech.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TicTacToeMain extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String name  = "TicTacToeView.fxml";
+        Parent root = FXMLLoader.load(getClass().getResource(name));
+        stage.setScene( new Scene(root));
+        stage.setTitle("TicTacToe by: Jorge, Calle");
+        stage.show();
+        stage.setHeight(774);
+        stage.setWidth(682);
+        stage.setResizable(false);
+    }
+}
---
+++ target/classes/edu/citytech/tictactoe/abccounter/model/Mode.java
@@ -0,0 +1,33 @@
+package edu.citytech.tictactoe.abccounter.model;
+
+public class Mode {
+    private String mode;
+    private String description;
+
+    public Mode(String mode, String description) {
+        this.mode = mode;
+        this.description = description;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+}
